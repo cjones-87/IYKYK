4684b6a300a9138b9e7b7889b0adf9ad07ac089c
CJ <cliftonjones.87@gmail.com>
2022-10-01 02:53:31 -0400

    feature: added home page and navigation bar

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="centered">
+      <h2>All I Know Is</h2>
+      <h1>
+        <big>
+          <b>IYKYK</b>
+        </big>
+      </h1>
+      <h2>Quiz Game</h2>
+    </div>
+  );
+};
+
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 import Anime from './components/anime/Anime';
 import Comics from './components/comics/Comics';
 import Music from './components/music/Music';
@@ -11,8 +13,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Navbar />
     <BrowserRouter>
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/anime" element={<Anime />} />
         <Route path="/comics" element={<Comics />} />
         <Route path="/music" element={<Music />} />
