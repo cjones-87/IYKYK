d6db7f7d7e102bfe826c8367f569f88c3d2db1ed
CJ <cliftonjones.87@gmail.com>
2022-10-03 02:16:49 -0400

    feature: added temp landing page

---
+++ src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+  return <div className="centered">Landing Page</div>;
+};
+
+export default LandingPage;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/navbar/Navbar';
+import LandingPage from './components/landingPage/LandingPage';
 import Home from './components/home/Home';
 import Anime from './components/anime/Anime';
 import Comics from './components/comics/Comics';
@@ -17,6 +18,7 @@ root.render(
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/anime" element={<Anime />} />
         <Route path="/comics" element={<Comics />} />
