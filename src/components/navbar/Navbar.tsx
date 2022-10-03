219a8ea8b643e147c62b93e2d7047b976fa1338b
CJ <cliftonjones.87@gmail.com>
2022-10-03 00:55:32 -0400

    feature: refactored navbar to be responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { NavLink } from 'react-router-dom';
+// import './Navbar.css';
 
 const Navbar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
   return (
-    <div className="navbar">
-      <a href="/home" style={{ lineHeight: '6rem' }}>
-        Home
-      </a>
-      <a href="/anime" style={{ lineHeight: '6rem' }}>
-        Anime
-      </a>
-      <a href="/comics" style={{ lineHeight: '6rem' }}>
-        Comics
-      </a>
-      <a href="/music" style={{ lineHeight: '6rem' }}>
-        Music
-      </a>
-      <a href="/vehicles" style={{ lineHeight: '6rem' }}>
-        Vehicles
-      </a>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink to="/" className="nav-logo">
+            All I Know Is, IYKYK &nbsp;&nbsp;
+            <i className="fas fa-gamepad"></i>
+          </NavLink>
+
+          <ul className={isClicked ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <NavLink
+                to="/home"
+                className={({ isActive }) =>
+                  isActive ? 'nav-links active' : 'nav-links'
+                }
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/anime"
+                className={({ isActive }) =>
+                  isActive ? 'nav-links active' : 'nav-links'
+                }
+                onClick={handleClick}
+              >
+                Anime
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/comics"
+                className={({ isActive }) =>
+                  isActive ? 'nav-links active' : 'nav-links'
+                }
+                onClick={handleClick}
+              >
+                Comics
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/music"
+                className={({ isActive }) =>
+                  isActive ? 'nav-links active' : 'nav-links'
+                }
+                onClick={handleClick}
+              >
+                Music
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/vehicles"
+                className={({ isActive }) =>
+                  isActive ? 'nav-links active' : 'nav-links'
+                }
+                onClick={handleClick}
+              >
+                Vehicles
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={isClicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
