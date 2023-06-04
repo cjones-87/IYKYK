49b72e614efee17d3262e1d19c1cc897843fcfad
CJ <cliftonjones.87@gmail.com>
2023-06-04 01:29:43 -0400

    feature: added header to improve ui

--- src/components/cartoons/Cartoons.tsx
+++ src/components/cartoons/Cartoons.tsx
@@ -1,7 +1,7 @@
 import Game from '../reusableComponents/Game';
 
 const Cartoons = () => {
-  return <Game category={32} type="multiple" />;
+  return <Game category={32} header={'Cartoons'} type="multiple" />;
 };
 
 export default Cartoons;
