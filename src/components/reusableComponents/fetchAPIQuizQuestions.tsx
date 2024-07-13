6a7be83adbea3c9b2c1504f8a703442af679e099
CJ <cliftonjones.87@gmail.com>
2024-07-13 15:21:22 -0400

    refactor: added in notification when a request is invalid to improve UX and notify that another choice must be made

--- src/components/reusableComponents/fetchAPIQuizQuestions.tsx
+++ src/components/reusableComponents/fetchAPIQuizQuestions.tsx
@@ -1,14 +1,21 @@
-import { Difficulty, Question, shuffleArray } from '../../utils/utils';
+import { Question, shuffleArray } from '../../utils/utils';
 
 export const fetchAPIQuizQuestions = async (
   amount: number,
-  difficulty: Difficulty,
+  difficulty: string,
   category: number,
   type: string
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`;
 
   const data = await (await fetch(endpoint)).json();
+
+  if (!data.results.length) {
+    alert(
+      'This quiz type or difficulty setting is not available for this quiz category. Please change either difficulty or quiz type to continue'
+    );
+    window.location.reload();
+  }
 
   return data.results.map((question: Question) => ({
     ...question,
