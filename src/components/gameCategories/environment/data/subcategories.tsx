3815abf96c70665050fb2e5a36740d13df72dd51
CJ <cliftonjones.87@gmail.com>
2024-07-14 12:31:58 -0400

    feature: added array object of sub-categories to assist with navbar & home page navigation

---
+++ src/components/gameCategories/environment/data/subcategories.tsx
@@ -0,0 +1,12 @@
+const subcategories = [
+  {
+    content: 'Animals',
+    href: '/environment/animals',
+  },
+  {
+    content: 'Geography',
+    href: '/environment/geography',
+  },
+];
+
+export default subcategories;
